807766862c9da5033304fcb8ff5dff640fc6c805
Casa2011 <casagmitryi@gmail.com>
2016-09-11 21:12:00 +0300

    cmt

--- esp/init.lua
+++ esp/init.lua
@@ -1,44 +1,59 @@
---t = require("ds18b20")
  
+--t = require("ds18b20") 
+
+gpio.mode(5, gpio.OUTPUT)
+gpio.write(5, gpio.LOW)
+-----------------------
 gpio.mode(6, gpio.OUTPUT)
 gpio.write(6, gpio.LOW)
+-----------------------
 gpio.mode(7, gpio.OUTPUT)
 gpio.write(7, gpio.LOW)
-
+-----------------------
+gpio.mode(8, gpio.OUTPUT)
+gpio.write(8, gpio.LOW)
 local function server_udp(client,data)
 CMD = data
---tmr.delay(10) 
+tmr.delay(10) 
 client:send ( CMD )
 print("rxData: ",CMD)
-tmr.delay(10) 
+tmr.delay(10)  
 
-
-if CMD=="A+" then
-        gpio.write(6, gpio.HIGH)
+if     CMD=="A+" then
+        gpio.write(5, gpio.HIGH)
 elseif CMD=="A-" then
-        gpio.write(6, gpio.LOW)
+        gpio.write(5, gpio.LOW)
 elseif CMD=="B+" then
-        gpio.write(7, gpio.HIGH)
+        gpio.write(6, gpio.HIGH)
 elseif CMD=="B-" then
+        gpio.write(6, gpio.LOW)
+elseif CMD=="C+" then
+        gpio.write(7, gpio.HIGH)
+elseif CMD=="C-" then
         gpio.write(7, gpio.LOW)
+elseif CMD=="D+" then
+        gpio.write(8, gpio.HIGH)
+elseif CMD=="D-" then
+        gpio.write(8, gpio.LOW)
 elseif CMD=="on" then
+        gpio.write(5, gpio.HIGH)
         gpio.write(6, gpio.HIGH)
         gpio.write(7, gpio.HIGH)
+        gpio.write(8, gpio.HIGH)
 elseif CMD=="off" then
+        gpio.write(5, gpio.LOW)
         gpio.write(6, gpio.LOW)
         gpio.write(7, gpio.LOW)
-end
-    
-   
-end
-
-
- 
-
+        gpio.write(8, gpio.LOW)
+        elseif CMD=="t" then 
+          tmr.delay(10) 
+        -- client:send (t.read()) 
+        -- print(t.read())       
+end      
+end 
 wifi.setmode(wifi.STATION)
 wifi.sta.config("LOG","PASS")
 wifi.sta.connect()
-
 i=0
 tmr.alarm(1, 1000, 1, function()
     if (wifi.sta.status() ~= 5 and i < 10) then
@@ -50,13 +65,9 @@ tmr.alarm(1, 1000, 1, function()
           print("IP:"..wifi.sta.getip())
        else
           print("Status:"..wifi.sta.status())
-       end
+              end
     end
 end)
-
-
-
 svr = net.createServer(net.UDP)
 svr:on("receive", server_udp)
-
 svr:listen (8080)
